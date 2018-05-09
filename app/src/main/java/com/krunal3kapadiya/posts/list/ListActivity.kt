39f6160986970678b5996a9bd56d75b89b54c605
DESKTOP-PJ4JQGO\Krunal Kapadiya <krunal.multidots@gmail.com>
2018-05-09 10:07:05 +0530

    validation for internet

--- app/src/main/java/com/krunal3kapadiya/posts/list/ListActivity.kt
+++ app/src/main/java/com/krunal3kapadiya/posts/list/ListActivity.kt
@@ -1,9 +1,14 @@
 package com.krunal3kapadiya.posts.list
 
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
+import android.util.Log
 import com.common.base.BaseActivity
+import com.common.utils.Utils
 import com.krunal3kapadiya.posts.Injection
 import com.krunal3kapadiya.posts.R
 import com.krunal3kapadiya.posts.common.models.Comments
@@ -27,6 +32,11 @@ import kotlinx.android.synthetic.main.activity_list.*
 class ListActivity : BaseActivity(), ListAdapter.onListItemClick {
 
     private lateinit var disposable: CompositeDisposable
+    private val LOG = ListActivity::class.java.name
+
+    lateinit var listViewModel: ListViewModel
+    lateinit var postUserComments: List<PostUserComments>
+    lateinit var adapter: ListAdapter
 
     /**
      * on create initialize needed variables
@@ -38,8 +48,43 @@ class ListActivity : BaseActivity(), ListAdapter.onListItemClick {
         //initializing
         disposable = CompositeDisposable()
         val viewModelFactory = Injection.provideListViewModel(this)
-        val listViewModel = ViewModelProviders.of(this, viewModelFactory).get(ListViewModel::class.java)
+        listViewModel = ViewModelProviders.of(this, viewModelFactory).get(ListViewModel::class.java)
 
+        //get the total number of comments(in list)
+        postUserComments = ArrayList()
+        adapter = ListAdapter(this, postUserComments)
+
+        rvPosts.layoutManager = LinearLayoutManager(this)
+        rvPosts.adapter = adapter
+
+        checkForInternet()
+
+        // on swipe fetch and display data
+        swipePosts.setOnRefreshListener({
+            fetchAndDisplayData()
+            swipePosts.isRefreshing = false
+        })
+
+        // initially fetch and display data
+        fetchAndDisplayData()
+    }
+
+    /**
+     * checking for the internet
+     */
+    private fun checkForInternet() {
+        if (!Utils.isNetworkAvailable(this@ListActivity)) {
+            Snackbar.make(swipePosts, getString(R.string.text_connect_internet), Snackbar.LENGTH_INDEFINITE).setAction(getString(R.string.text_go_to_settings), {
+                startActivity(Intent(Settings.ACTION_WIRELESS_SETTINGS));
+            }).show()
+        }
+    }
+
+    /**
+     * initially it will load the data
+     * and in swipe refresh event also
+     */
+    private fun fetchAndDisplayData() {
         // post api object
         val postApi: PostApi = NetworkModule.providePostsApi()
 
@@ -52,7 +97,7 @@ class ListActivity : BaseActivity(), ListAdapter.onListItemClick {
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe())
-                })
+                }, { Log.e(LOG, getString(R.string.error_string)) })
 
         // user observer
         val userObservable: Observable<List<Users>> = postApi.getUsers()
@@ -63,7 +108,7 @@ class ListActivity : BaseActivity(), ListAdapter.onListItemClick {
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe())
-                })
+                }, { Log.e(LOG, getString(R.string.error_string)) })
 
         // comment observer
         val commentObservable: Observable<List<Comments>> = postApi.getComments()
@@ -74,20 +119,13 @@ class ListActivity : BaseActivity(), ListAdapter.onListItemClick {
                             .subscribeOn(Schedulers.io())
                             .observeOn(AndroidSchedulers.mainThread())
                             .subscribe())
-                })
+                }, { Log.e(LOG, getString(R.string.error_string)) })
 
-
-        //get the total number of comments(in list)
-        val postUserComments: List<PostUserComments> = ArrayList()
-        val adapter = ListAdapter(this, postUserComments)
         listViewModel.getListData().observe(this, android.arch.lifecycle.Observer { postUserCommentsList ->
             (postUserComments as ArrayList<PostUserComments>).clear()
-            postUserComments.addAll(postUserCommentsList as ArrayList)
+            (postUserComments as ArrayList<PostUserComments>).addAll(postUserCommentsList as ArrayList)
             adapter.notifyDataSetChanged()
         })
-
-        rvPosts.layoutManager = LinearLayoutManager(this)
-        rvPosts.adapter = adapter
     }
 
     /**
